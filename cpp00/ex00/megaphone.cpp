866ce1607b15c0b0089d824f51f83637e2259948
root <root@DESKTOP-7CO9QQK>
2023-12-15 16:28:01 +0100

    cpp00/ex00 done

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+char	**convert_to_capital(int ac, char **av){
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < ac) {
+		j = 0;
+		while (av[i][j]) {
+			av[i][j] = toupper(av[i][j]);
+			j++;
+		}
+		i++;
+	}
+	return (av);
+}
+
+int main(int ac, char **av) {
+	int	i;
+
+	if (ac == 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	i = 1;
+	av = convert_to_capital(ac, av);
+	while (i < ac - 1) {
+		std::cout << av[i] << " ";
+		i++;
+	}
+	std::cout << av[i] << std::endl;
+    return (0);
 }
