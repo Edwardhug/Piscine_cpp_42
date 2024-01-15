7b310d968c717babdba8f77222169347c5b16a22
root <root@DESKTOP-7CO9QQK>
2024-01-15 15:56:26 +0100

    cpp01ex04 done

--- cpp01/ex04/srcs/main.cpp
+++ cpp01/ex04/srcs/main.cpp
@@ -4,6 +4,25 @@
 
 using namespace std;
 
+void	read_and_replace(char **av, ifstream &source_fd, ofstream &dest_fd) {
+	string	line;
+	size_t	lenght_to_remove;
+	size_t	i;
+
+	lenght_to_remove = strlen(av[2]);
+	while (getline(source_fd, line)) {	//read source line by line
+		i = line.find(av[2]);
+		while (i != string::npos) {		//check if line.find find something
+			line.erase(i, lenght_to_remove);	//remove av[2]
+			line.insert(i, av[3]);				//insertav[3]
+			i = line.find(av[2]);
+		}
+		dest_fd << line;
+		if (!source_fd.eof())
+			dest_fd << endl;
+	}
+}
+
 int	file_copy(char **av) {
 	string new_file = string(av[1]) + ".replace";
 
@@ -20,7 +39,7 @@ int	file_copy(char **av) {
         return 1;
 	}
 
-	dest_fd << source_fd.rdbuf();	//writting
+	read_and_replace(av, source_fd, dest_fd);
 
 	dest_fd.close();			//closing
 	source_fd.close();			//closing
@@ -36,6 +55,5 @@ int	main(int ac, char **av) {
 		cout << "Error during the copy" << endl;
 		return (1);
 	}
-	cout << "No problem" << endl;
 	return (0);
 }
