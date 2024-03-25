98b2df55e3869cda3daf11feaa04f1a19a5e59ba
Lucien Gabet <lgabet@z4r2p7.42lyon.fr>
2024-03-25 11:39:18 +0100

    cpp04 done

--- cpp04/ex02/srcs/main.cpp
+++ cpp04/ex02/srcs/main.cpp
@@ -8,21 +8,23 @@ int main()
 
         for (int i = 0; i < 10; i++)
         {
-                if (i >= 5)
+                if (i >= 5) {
                         animals[i] = new Dog();
+                        // animals[i] = new Animal();
+                }
                 else
                         animals[i] = new Cat();
         }
         std::cout << std::endl;
 
         int num = 1;
-		Cat	*Cat_ptr = dynamic_cast<Cat*>(animals[num]);
-		if (Cat_ptr)
-		{
-			std::cout << "printing an idea animals " << num << " thinks ";
-			Cat_ptr->printThought(1);
-			std::cout << std::endl;
-		}
+	Cat	*Cat_ptr = dynamic_cast<Cat*>(animals[num]);
+	if (Cat_ptr)
+	{
+		std::cout << "printing an idea animals " << num << " thinks ";
+		Cat_ptr->printThought(1);
+		std::cout << std::endl;
+	}
 
         std::cout << std::endl;
         for (int i = 0; i < 10; i++)
