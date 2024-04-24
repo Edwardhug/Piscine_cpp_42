b5701c811e14c30f23722327600db30cea67d056
Lucien Gabet <lgabet@z4r10p7.42lyon.fr>
2024-04-24 12:06:22 +0200

    just added more test cpp07ex002

--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -4,11 +4,47 @@
 #include <time.h>
 
 #define MAX_VAL 750
+
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
 int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
+    Array<Awesome> test(15);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
+
+    try {
+        std::cout << "complex test 5 = " << test[5] << std::endl;
+        numbers[1] = 42;
+        std::cout << "accessor test 1 = " << numbers[1] << std::endl;
+        numbers[1] = 43;
+        std::cout << "accessor test 2 = " << numbers[1] << std::endl;
+
+    } catch (const std::exception &e) {
+        std::cerr << "error during added test " << e.what() << std::endl;
+    }
+
     for (int i = 0; i < MAX_VAL; i++)
 	{
         const int value = rand();
@@ -17,8 +53,7 @@ int main(int, char**)
     }
     //SCOPE
     {
-        Array<int> tmp;
-        tmp = numbers;
+        Array<int> tmp = numbers;
         Array<int> test(tmp);
     }
 
