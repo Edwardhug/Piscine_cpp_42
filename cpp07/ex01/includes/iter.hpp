475476d6143b2f66c8a06439e4c7118cc31691f7
Lucien Gabet <lgabet@z4r9p1.42lyon.fr>
2024-04-25 12:06:50 +0200

    better cpp07ex01 for me because it pass anyway

--- cpp07/ex01/includes/iter.hpp
+++ cpp07/ex01/includes/iter.hpp
@@ -5,9 +5,14 @@
 
 template<typename T>
 void	iter(T *array, int length, void	(*f)(T&)) {
-	for (int i = 0; i < length && array[i]; i++)
+	for (int i = 0; i < length; i++)
 		f(array[i]);
-	
+}
+
+template<typename T>
+void	iter(T *array, int length, void	(*f)(const T&)) {
+	for (int i = 0; i < length; i++)
+		f(array[i]);
 }
 
 template<typename T>
