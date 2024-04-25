3151282e4c16e980bc83483dfeeecc852be19204
Lucien Gabet <lgabet@z4r9p1.42lyon.fr>
2024-04-25 14:41:42 +0200

    need to do some test but cpp08 seems to be done

---
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -0,0 +1,41 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+	public :
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+
+	MutantStack() {};
+	~MutantStack() {};
+	MutantStack(const MutantStack &copy) {*this = copy;};
+	MutantStack<T> &operator=(const MutantStack &copy) {this->operator=(copy); return(this);};
+
+	iterator begin() {
+		return (this->c.begin());
+	};
+	const_iterator cbegin() const {
+		return (this->c.begin());
+	};
+	reverse_iterator rbegin() {
+		return (this->c.rbegin());
+	};
+
+	iterator end() {
+		return (this->c.end());
+	};
+	const_iterator cend() const {
+		return (this->c.cend());
+	};
+	reverse_iterator rend() {
+		return (this->c.rend());
+	};
+};
+
+#endif
