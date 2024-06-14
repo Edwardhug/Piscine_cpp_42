8d395c0df6d6bdad0613a5cadf99b90259b0a307
Lucien Gabet <lgabet@z4r8p1.42lyon.fr>
2024-06-14 11:56:58 +0200

    trying to fix cpp09ex00

--- cpp09/ex00/srcs/BitcoinExchange.cpp
+++ cpp09/ex00/srcs/BitcoinExchange.cpp
@@ -1,6 +1,8 @@
 #include "../includes/BitcoinExchange.hpp"
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <sstream>
 
 BitcoinExchange::BitcoinExchange() {}
 
@@ -11,11 +13,13 @@ BitcoinExchange::BitcoinExchange(const BitcoinExchange &copy) {
 }
 
 BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &copy) {
-	*this = copy;
+	this->_data = copy._data;
 	return *this;
 }
 
-int	stoi(const std::string &str) {
+//	UTILS
+
+int	ft_stoi(const std::string &str) {
 	int result = 0;
 	for (size_t i = 0; i < str.size(); i++) {
 		result = result * 10 + str[i] - '0';
@@ -23,7 +27,25 @@ int	stoi(const std::string &str) {
 	return result;
 }
 
-float	stof(const std::string &str) {
+// char	*ft_itoa(const int num) {
+// 	std::cout << "num = " << num << std::endl;
+// 	char ret[100];
+
+// 	int i = 0;
+// 	int	size = 0;
+// 	while (num / (size * 10) > 0){
+// 		size++;
+// 	}
+// 	while (i > size) {
+// 		ret[i] = (num / (size - 1)) + '0';
+// 		i++;
+// 	}
+// 	ret[i] = 0;
+// 	std::cout << "ret = " << ret << std::endl;
+// 	return ret;
+// }
+
+float	ft_stof(const std::string &str) {
 	float result = 0;
 	int i = 0;
 	while (str[i] && str[i] != '\n' && str[i] != '.') {
@@ -42,31 +64,6 @@ float	stof(const std::string &str) {
 	return result;
 }
 
-void BitcoinExchange::fill(const char *filename) {
-	std::ifstream file(filename);
-	if (!file.is_open()) {
-		std::cerr << "Error: could not open file" << std::endl;
-		return;
-	}
-	int year, month, day;
-	int timestamp;
-	float price;
-	std::string line;
-	std::getline(file, line);
-	while (std::getline(file, line)) {
-		year = stoi(line.substr(0, 4));
-		month = stoi(line.substr(5, 2));
-		day = stoi(line.substr(8, 2));
-		price = stof(line.substr(11));
-		timestamp = year * 10000 + month * 100 + day;
-		_data[timestamp] = price;
-	}
-	file.close();
-}
-
-std::map<int, float> BitcoinExchange::getData() const {
-	return _data;
-}
 
 bool checkDate(int year, int month, int day) {
 	if (year < 2009 || year > 2024) {
@@ -84,6 +81,35 @@ bool checkDate(int year, int month, int day) {
 	return true;
 }
 
+// METHODES
+
+void BitcoinExchange::fill(const char *filename) {
+	std::ifstream file(filename);
+	if (!file.is_open()) {
+		std::cerr << "Error: could not open file" << std::endl;
+		return;
+	}
+	int year, month, day;
+	int timestamp;
+	float price;
+	std::string line;
+	std::getline(file, line);
+	while (std::getline(file, line)) {
+		year = ft_stoi(line.substr(0, 4));
+		month = ft_stoi(line.substr(5, 2));
+		day = ft_stoi(line.substr(8, 2));
+		price = ft_stof(line.substr(11));
+		timestamp = year * 10000 + month * 100 + day;
+		_data[timestamp] = price;
+	}
+	file.close();
+}
+
+std::map<int, float> BitcoinExchange::getData() const {
+	return _data;
+}
+
+
 void BitcoinExchange::getPrice(const char *date) const {
 	std::ifstream file(date);
 	if (!file.is_open()) {
@@ -97,11 +123,13 @@ void BitcoinExchange::getPrice(const char *date) const {
 			std::cerr << "Error: invalid format" << std::endl;
 		}
 		else {
-		int year = stoi(line.substr(0, 4));
-		int month = stoi(line.substr(5, 2));
-		int day = stoi(line.substr(8, 2));
-		float number = stof(line.substr(13));
-		if (checkDate(year, month, day) == false) {
+		int year = ft_stoi(line.substr(0, 4));
+		int month = ft_stoi(line.substr(5, 2));
+		int day = ft_stoi(line.substr(8, 2));
+		float number = ft_stof(line.substr(13));
+		if (number < 0)
+			std::cerr << "Error: not a positive number." << std::endl;
+		else if (checkDate(year, month, day) == false) {
 			std::cerr << "Error: invalid date " << year << "-" << month << "-" << day << std::endl;
 		}
 		else {
@@ -110,12 +138,20 @@ void BitcoinExchange::getPrice(const char *date) const {
 				std::cout << year << "-" << month << "-" << day << "=> " << this->getData()[timestamp] * number << std::endl;
 			}
 			else {
-				std::map<int, float>::const_iterator it = this->getData().begin();
+				std::map<int, float>::const_iterator it;
+				while (this->getData()[timestamp] == 0 && timestamp < 20220329)
+					timestamp++;
+				it = this->getData().find(timestamp);
 				it++;
-				std::cout << "key: " << it->first << " value: " << it->second << std::endl;
-				
+				std::ostringstream odate;
+				odate << it->first / 10000 << "-" << (it->first / 100) % 100 << "-" << (it->first % 10);
+				std::string	date = odate.str();
+				// std::string date = std::to_string(it->first / 10000) + " - " + std::to_string(it->first / 100);
+				// std::cout << "DATE = " << date << std::endl;
+				std::cout << date << "=> "<< it->second << std::endl;
 			}
 		}
 		}
 	}
+	file.close();
 }
