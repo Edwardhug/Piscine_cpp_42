dce40514df00b06b4f07565aad7bcfbbf69f670a
Lucien Gabet <lgabet@z4r9p1.42lyon.fr>
2024-06-18 11:23:04 +0200

    cpp09ex01 done

--- cpp09/ex01/srcs/RPN.cpp
+++ cpp09/ex01/srcs/RPN.cpp
@@ -20,13 +20,11 @@ std::stack<int> Rpn::getStack() {
 void	Rpn::polishNote(char *av) {
 	for (int i = 0; av[i] && av[i + 1]; i++) {
 		if ((av[i] >= '0' && av[i] <= '9') && (av[i + 1] >= '0' && av[i + 1] <= '9')) {
-			std::cout << "Error: Number above 9." << std::endl;
 			return ;
 		}
 	}
 	for (int i = 0; av[i] && av[i + 1]; i++) {
 		if (av[i] != ' ' && av[i] != '+' && av[i] != '-' && av[i] != '/' && av[i] != '*' && (av[i] < '0' || av[i] > '9')) {
-			std::cout << "Error: Bad character." << std::endl;
 			return ;
 		}
 	}
@@ -39,25 +37,23 @@ void	Rpn::polishNote(char *av) {
 		else if (av[i] == '+' || av[i] == '-' || av[i] == '/' || av[i] == '*') {
 			if (_stack.size() == 1) {
 				_stack.pop();
-				std::cout << "Error: Not enough number to do the operation." << std::endl;
 				return ;
 			}
 			temp = _stack.top();
 			_stack.pop();
 			if (av[i] == '+')
-				temp = temp + _stack.top();
+				temp = _stack.top() + temp;
 			else if (av[i] == '-')
-				temp = temp - _stack.top();
+				temp = _stack.top() - temp;
 			else if (av[i] == '*')
-				temp = temp * _stack.top();
+				temp = _stack.top() * temp;
 			else if (av[i] == '/')
-				temp = temp / _stack.top();
+				temp = _stack.top() / temp;
 			_stack.pop();
 			_stack.push(temp);
 		}
 	}
 	if (_stack.size() != 1) {
-		std::cout << "Error: Too much number at the end." << std::endl;
 		while (_stack.size() != 0)
 			_stack.pop();
 		return ;
--- cpp09/ex01/srcs/main.cpp
+++ cpp09/ex01/srcs/main.cpp
@@ -2,13 +2,13 @@
 
 int main(int ac, char **av) {
 	if (ac != 2) {
-		std::cout << "Error :wrong number of arg." << std::endl;
+		std::cout << "Error: Wrong number of arg." << std::endl;
 		return 1;
 	}
 	Rpn polish;
 	polish.polishNote(av[1]);
 	if (polish.getStack().empty() == true)
-		std::cout << "Error :Wrong arg." << std::endl;
+		std::cout << "Error: Wrong arg." << std::endl;
 	else
 		std::cout << "Result = " << polish.getStack().top() << std::endl;
 }
